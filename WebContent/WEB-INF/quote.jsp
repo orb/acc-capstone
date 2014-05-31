3108f6d6851969992a6a7fcc6b5c1a23bcc8db1d
Norman <orb@nostacktrace.com>
2014-05-31 16:50:16 -0500

    day two

--- WebContent/WEB-INF/quote.jsp
+++ WebContent/WEB-INF/quote.jsp
@@ -1,5 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,17 +16,31 @@
 		<h1>The Great Quote Machine</h1>
 	</div>
 
+
 	<div class="container">
-		<blockquote>
-			<p>${quote.text}</p>
-			<footer>${quote.author}</footer>
-		</blockquote>
+		<c:forEach var="quote" items="${quotes}">
+			<blockquote>
+				<p>${quote.text}</p>
+				<footer>${quote.author}</footer>
+			</blockquote>
+		</c:forEach>
+	</div>
+
+	<div class="container">
+		<footer>
+			<p>The quote database contains ${fn:length(quotes)} quotes as of
+				<fmt:formatDate value="${now}" pattern="E h:m a"/>
+			</p>
+		</footer>
 	</div>
 
 
 	<script src="/js/bootstrap.js"></script>
 </body>
 </html>
+
+
+
 
 
 
--- src/edu/austincc/capstone/QuoteServlet.java
+++ src/edu/austincc/capstone/QuoteServlet.java
@@ -1,7 +1,8 @@
 package edu.austincc.capstone;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,19 +13,42 @@ import javax.servlet.http.HttpServletResponse;
 public class QuoteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public Quote makeQuote() {
+	public Quote quote1() {
 		return new Quote("Logic will get you from A to B. Imagination will take you everywhere.",
 				"Albert Einstein");
 	}
 	
+	public Quote quote2() {
+		return new Quote("This is a quote", "Norman");
+	}
+	
+	public Quote quote3() {
+		return new Quote("I like turtles", "Some kid on the internet");
+	}
+	
+	public Quote[] allQuotes() {
+		return new Quote[] {quote1(), quote2(), quote3()};
+	}
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException 
 	{
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/quote.jsp");
-		request.setAttribute("quote", makeQuote());
-		dispatcher.forward(request, response);
+		request.setAttribute("quotes", allQuotes());
+		request.setAttribute("now", new Date());
+
+		request.getRequestDispatcher("/WEB-INF/quote.jsp").forward(request, response);
 	}
 }
+
+
+
+
+
+
+
+
+
 
 
 
