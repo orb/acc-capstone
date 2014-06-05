78e1edcb8cdc683a21f3510303adee52f642af31
Norman <orb@nostacktrace.com>
2014-06-05 08:56:19 -0500

    cleanup

--- db/data.sql
+++ db/data.sql
@@ -3,3 +3,5 @@ INSERT INTO QUOTE (text, author) VALUES
 ('Testing can only prove the presence of bugs, not their absence.', 'Edsger W. Dijkstra'),
 ('The best way to predict the future is to implement it.', 'Alan Kay'),
 ('There are two ways to write error-free programs; only the third one works.', 'Alan Perlis');
+
+--"Logic will get you from A to B. Imagination will take you everywhere.", "Albert Einstein"
