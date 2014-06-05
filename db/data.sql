ef67e8281f5501d5dc7132f24c58564609753b6d
Norman <orb@nostacktrace.com>
2014-06-04 22:09:37 -0500

    add schema

---
+++ db/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO QUOTE (text, author) VALUES
+('Any fool can write code that a computer can understand. Good programmers write code that humans can understand', 'Martin Fowler'),
+('Testing can only prove the presence of bugs, not their absence.', 'Edsger W. Dijkstra'),
+('The best way to predict the future is to implement it.', 'Alan Kay'),
+('There are two ways to write error-free programs; only the third one works.', 'Alan Perlis');
---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE quote (
+    id     IDENTITY,
+    text   VARCHAR (100),
+    author VARCHAR (100)
+);
